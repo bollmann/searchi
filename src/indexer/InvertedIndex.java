453f803b4df86f4a9cc159f723e5ffd6217da136
Dominik Bollmann <dominikbollmann@gmail.com>
2015-11-26 17:53:17 -0500

    condense build.xml

--- src/indexer/InvertedIndex.java
+++ src/indexer/InvertedIndex.java
@@ -11,7 +11,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 
--- src/indexer/offline/DocumentReader.java
+++ src/indexer/offline/DocumentReader.java
@@ -13,8 +13,6 @@ import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 import com.google.gson.Gson;
 
