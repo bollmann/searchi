7ebf633835c25d4da9d6af740723be0dab8a4a3d
Ishan Srivastava <ishan@seas.upenn.edu>
2015-11-26 18:26:16 -0500

    Adding URLInputFormat to read from JSON in Init phase of PageRank. Also adding logic for dealing with self-links

--- src/pagerank/phase1/PRInitReducer.java
+++ src/pagerank/phase1/PRInitReducer.java
@@ -7,12 +7,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class PRInitReducer extends Reducer<Text, Text, Text, Text> {
 
+	private static final String DELIM_PR_LINKS = "###";
+	
 	@Override
 	public void reduce(Text key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
 		
 		String initPageRank = "1.0";
-		StringBuilder outVal = new StringBuilder(initPageRank + "#");
+		StringBuilder outVal = new StringBuilder(initPageRank + DELIM_PR_LINKS);
 		
 		for (Text value : values) {
 			outVal.append(" ");
