10ebb7d29ebf2955985858946a733d0bbf0f32b0
Ishan Srivastava <ishan@seas.upenn.edu>
2015-11-30 12:49:56 -0500

    Removing filtering of self links from Mapper to URLsFromJsonReader. Also augmenting the StringUtils method to concatenate list to string with a filter

--- src/pagerank/phase1/PRInitMapper.java
+++ src/pagerank/phase1/PRInitMapper.java
@@ -1,13 +1,9 @@
 package pagerank.phase1;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
-import utils.string.StringUtils;
 
 public class PRInitMapper extends Mapper<Text, Text, Text, Text> {
 
@@ -15,18 +11,18 @@ public class PRInitMapper extends Mapper<Text, Text, Text, Text> {
 	public void map(Text key, Text value, Context context) 
 		throws IOException, InterruptedException {
 		
-		/* Removing self links */
-		String [] outLinks = value.toString().split(" ");
+		/* Removing self links - Moved to URLsFromJsonReader*/
+		/*String [] outLinks = value.toString().split(" ");
 		List<String> updOutLinks = new ArrayList<>();
 		String srcLink = key.toString().trim();
 		for (int i = 0; i < outLinks.length; ++i) {
 			if (!outLinks[i].trim().equals(srcLink)) {
 				updOutLinks.add(outLinks[i].trim());				
 			}
-		}
+		}*/
 		
 		/* Map output in format L1		L2 L3 L5 L7 */		
-		context.write(key, new Text(StringUtils.listToString(updOutLinks, " ")));
+		context.write(key, new Text(value));
 	}
 	
 	
--- src/utils/string/StringUtils.java
+++ src/utils/string/StringUtils.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 public final class StringUtils {
 
-	/** Converts the list of links to space delimited string */
+	/** Converts the list of links to space delimited string  */
 	public static String listToString(List<String> outgoingLinks,
-			String delimiter) {
+			String delimiter, List<String> filter) {
 
 		StringBuffer strBuf = new StringBuffer();
 		boolean isFirst = true;
 		for (String link : outgoingLinks) {
+			if (filter != null && filter.contains(link.trim())) {
+				continue;
+			}
 			if (!isFirst) {
 				strBuf.append(delimiter);
 			}
