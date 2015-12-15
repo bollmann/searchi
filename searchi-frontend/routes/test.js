be250e6e2755e3c712dfe7604c2cdc2a96f0bc1b
gouthamank <drool@158.130.111.135>
2015-12-15 15:54:26 -0500

    removed hardcoded IPs

--- searchi-frontend/routes/test.js
+++ searchi-frontend/routes/test.js
@@ -5,12 +5,7 @@ var jade = require('jade');
 var path = require('path');
 
 router.get('/', function(req, res, next) {
-	var url = "http://192.168.0.100:8080/searchInterface?q=obama";
-	request(url, function(err, resp, body){	
-		body = JSON.parse(body);
-		console.log(jade.renderFile(path.join(__dirname, '../views/seults.jade'), {a: 'a'}))
-		res.render('results', body);		
-	})
+	res.render('test');
 });
 
 module.exports = router;
